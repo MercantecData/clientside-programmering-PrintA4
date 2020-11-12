f3cc9b98ed8d36c12ebbfc61f2d8ac5f2d273a94
PrintA4 <68830386+PrintA4@users.noreply.github.com>
2020-11-12 14:41:01 +0100

    added a few more lines

--- Clientside programming hjemmeside/java.js
+++ Clientside programming hjemmeside/java.js
@@ -1,3 +1,26 @@
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 document.getElementById("time").innerHTML = date;
+
+
+function vindstyrkeMål() {
+    var enhed1 = document.getElementById("vind")
+    window.prompt("vind: ")
+    vind = parseFloat(vind)
+
+}
+
+function køretøjsVægt() {
+    var enhed2 = document.getElementById("vægt")
+    window.prompt("vægt: ")
+    vægt = parseFloat(vægt)
+    
+
+}
+
+function beregn() {
+    var tryk = document.getElementById("beregn")
+
+
+}
+
